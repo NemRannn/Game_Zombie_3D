c32a9e5520e514c4b2a2570ce0f98cd33067b3a1
tuananh230902 <tuananh230902@gmail.com>
2024-10-17 21:31:33 +0700

    Add dummy C# file to indicate project language

---
+++ Dummy.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DummyNamespace
+{
+    public class Dummy
+    {
+        public void DummyMethod()
+        {
+            Console.WriteLine("This is a dummy class to indicate C#.");
+        }
+    }
+}
